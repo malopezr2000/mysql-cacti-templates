c003564e704277678b35ef94284d37f97429b4e7
baron.schwartz <baron.schwartz@14bb9f40-0f49-0410-a1f6-25d4bd87bb24>
2008-04-26 17:23:50 +0000

    initial go at making templates from compact definitions with a Perl script

---
+++ trunk/cacti_host_template_x_db_server_ht.pl
@@ -0,0 +1,168 @@
+# This is the definition of a Cacti template.  This file should contain nothing
+# but a big Perl data structure.  Don't execute this file.  Instead, process it
+# with make-template.pl like this:
+#
+# perl make-template.pl <file>
+#
+# This program is copyright (c) 2007 Baron Schwartz. Feedback and improvements
+# are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.  On UNIX and similar systems, you can issue `man
+# perlgpl' to read this license.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+
+{  name => 'DB Server',
+   hash => 'hash_0200133df221ff7893faad48ecc15026687215',
+   inputs => {
+      'Get MySQL Stats' => {
+         name => 'Get MySQL Stats',
+         hash => 'hash_030013a7b8dc9089ba40cbb293524024e51129',
+         type_id => 1,
+         # TODO: port
+         input_string => '<path_php_binary> -q '
+            . '<path_cacti>/scripts/ss_get_mysql_stats.php <hostname> <username>'
+            . '<password>',
+         inputs => [
+            {  name => 'hostname',
+               hash => 'hash_070013c89cbc8b1c987e050a310ee5414b2a02',
+               allow_nulls => '',
+            },
+            {  name => 'username',
+               hash => 'hash_070013181ee907dbb2bbfbfb3c6611f44e5625',
+               allow_nulls => 'on',
+            },
+            {  name => 'password',
+               hash => 'hash_070013e4ad68877c6e87087e52785d0a870282',
+               allow_nulls => 'on',
+            },
+         ],
+         outputs => {
+            Key_read_requests => 'hash_070013b1c52bc482f979e5098096f60a346d96',
+            Key_reads => 'hash_070013744750ba74f76128825af0421da2fc5e',
+            Key_write_requests => 'hash_070013f883de2e830958a1b3472f1bb8209b16',
+            Key_writes => 'hash_070013edc5905b12c0765d8b244467bf90ef13',
+         },
+      },
+   },
+   graphs => [
+      {  name => 'MyISAM Indexes',
+	      hash => 'hash_000013c870e809c16ac409190bcfbaf6c55624',
+         items => [
+            {  item => 'Key_read_requests',
+               color => '157419',
+               type => 'AREA',
+               hashes => [qw(
+                  hash_100013ac260a1434298e088f15f70cd1a5f726
+                  hash_1000132880fe948b6912a3bbef6d572fc08c11
+                  hash_1000137ad6f41485c166ba32e6d9e570e395f0
+                  hash_100013c0e60c7d108ee721a537bb6e3c62a1d8
+               )],
+               task => 'hash_0900131d370e9a5914bc84d71e779814f357cb',
+            },
+            {  item => 'Key_reads',
+               color => 'AFECED',
+               hashes => [qw(
+                  hash_10001325df59422c805e4b85310ec3a4face3b
+                  hash_100013f8b4402537c5b8f6fe87f025b66fa36d
+                  hash_100013508445350edf7ebffcc53df8ca402cd4
+                  hash_100013ce2c5b685c988e9ba283a5b26019a228
+               )],
+               task => 'hash_0900135f6452acaf1e44638836412e2a701af5',
+            },
+            {  item => 'Key_write_requests',
+               color => 'F51D30',
+               hashes => [qw(
+                  hash_100013c8ddc4e379739fa6860644b9ec73fd67
+                  hash_100013eee6bf6b5627650df8625d88f333299f
+                  hash_100013c13f3d345b73c7333254c1d3efcbd02f
+                  hash_100013c1d97c83fc4b5cbed160c1530f937307
+               )],
+               task => 'hash_090013cf20ffcf42c91bb6f8e6d4cadc6b74a5',
+            },
+            {  item => 'Key_writes',
+               color => '862F2F',
+               hashes => [qw(
+                  hash_100013b05a1a789491c12bed93e2c610e72fa0
+                  hash_100013c5621ec923c0e47f0603638f9fbb5815
+                  hash_1000137e9cc68942f31c8de3788336f080166f
+                  hash_100013c6da7f77308d3127ca7e18385c86ceeb
+               )],
+               task => 'hash_090013b06844c326fac7ec50d9819b47b3430a',
+            },
+         ],
+         dt => {
+            hash => 'hash_01001329f0b43ffd723039d3428839ab9f5174',
+            input => 'Get MySQL Stats',
+            Key_read_requests => {
+               hash => 'hash_080013b407e61e9948460c99116f7f6a72cf92',
+               data => 'unsigned',
+               type => 'COUNTER',
+            },
+            Key_reads => {
+               hash => 'hash_0800130de2bf128eac0bc672d77085da38f8cf',
+               data => 'unsigned',
+               type => 'COUNTER',
+            },
+            Key_write_requests => {
+               hash => 'hash_080013039d69d0262c783f20b77fa3b3f62e88',
+               data => 'unsigned',
+               type => 'COUNTER',
+            },
+            Key_writes => {
+               hash => 'hash_080013f8851b46ed984a9e8fba454c48995230',
+               data => 'unsigned',
+               type => 'COUNTER',
+            },
+         },
+      },
+   ],
+   rras => [
+      {  hash           => 'hash_150013c21df5178e5c955013591239eb0afd47',
+         name           => 'Daily (5 Minute Average)',
+         x_files_factor => '0.5',
+         steps          => 1,
+         rows           => 600,
+         timespan       => 86400,
+         cf_items       => '1|3',
+      },
+      {  hash           => 'hash_1500130d9c0af8b8acdc7807943937b3208e30',
+         name           => 'Weekly (30 Minute Average)',
+         x_files_factor => '0.5',
+         steps          => '6',
+         rows           => '700',
+         timespan       => '604800',
+         cf_items       => '1|3',
+      },
+      {  hash           => 'hash_1500136fc2d038fb42950138b0ce3e9874cc61',
+         name           => 'Monthly (2 Hour Average)',
+         x_files_factor => '0.5',
+         steps          => '24',
+         rows           => '775',
+         timespan       => '2678400',
+         cf_items       => '1|3',
+      },
+      {  hash           => 'hash_150013e36f3adb9f152adfa5dc50fd2b23337f',
+         name           => 'Yearly (1 Day Average)',
+         x_files_factor => '0.5',
+         steps          => '288',
+         rows           => '797',
+         timespan       => '33053184',
+         cf_items       => '1|3',
+      },
+   ],
+   gprints => {
+      Normal => {
+         hash => 'hash_060013e9c43831e54eca8069317a2ce8c6f752',
+         gprint_text => '%8.2lf %s',
+      },
+   }
+};
---
+++ trunk/make-template.pl
@@ -0,0 +1,336 @@
+#!/usr/bin/perl
+
+# This is a script that generates Cacti templates from a Perl data structure.
+#
+# This program is copyright (c) 2007 Baron Schwartz. Feedback and improvements
+# are welcome.
+#
+# THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, version 2.  On UNIX and similar systems, you can issue `man
+# perlgpl' to read this license.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA.
+
+use strict;
+use warnings FATAL => 'all';
+use English qw(-no_match_vars);
+
+# TODO: command-line options
+# 64-bit max counter sizes for wrap detection
+# Graph size
+# polling interval
+# Name prefix -- default P
+# Number of samples to keep in the RRD files.
+
+my $name_prefix   = 'X';
+my $rrd_step      = 300;
+my $rrd_heartbeat = 600;
+my $rrd_maximum   = '18446744073709551615',
+# TODO: 4294967295
+
+# #############################################################################
+# Read the file given on the command line into a string and then into a
+# variable.
+# #############################################################################
+my ($filename) = shift @ARGV;
+open my $fh, '<', $filename or die "Can't open $filename: $OS_ERROR";
+my $code = do { local $INPUT_RECORD_SEPARATOR = undef; <$fh>; };
+close $fh;
+
+# This should be a hashref now.
+my $t = eval($code);
+if ( $EVAL_ERROR ) {
+   die $EVAL_ERROR;
+}
+
+# #############################################################################
+# Global variables.
+# #############################################################################
+my $indent = 0;
+my @stack  = ();
+my $hashno = 0;
+
+my %graph_types = (
+   LINE1 => 4,
+   AREA  => 7,
+   GPRINT => 9,
+);
+
+my @graph_texts = qw(UNUSED Current: Average: Max:); # TODO: shorten
+my @consolidations = qw(1 4 1 3);
+
+# The properties of a graph.
+my @graph_props = qw( title image_format_id height width auto_scale
+   auto_scale_opts auto_scale_log auto_scale_rigid auto_padding export
+   upper_limit lower_limit base_value unit_value unit_exponent_value
+   vertical_label);
+my %graph_props = (
+   image_format_id => 1,
+   height          => 120,
+   width           => 500,
+   auto_scale      => 'on',
+   auto_scale_opts => 2,
+   auto_padding    => 'on',
+   export          => 'on',
+   base_value      => 1000,
+);
+
+# The properties of a data source item.
+my @ds_props = qw(
+   data_source_name rrd_minimum rrd_maximum data_source_type_id rrd_heartbeat
+   data_input_field_id);
+my %ds_props = (
+   rrd_minimum         => 0,
+   rrd_maximum         => $rrd_maximum,
+   data_source_type_id => 2,
+   rrd_heartbeat       => $rrd_heartbeat,
+);
+
+# The properties of a graph item (a line, area, gprint, etc)
+my @gitem_props = qw(task_item_id color_id graph_type_id
+   consolidation_function_id cdef_id value gprint_id text_format hard_return
+   sequence);
+
+# Magically named fields for input scripts
+my $is_magic = qr{
+   hostname
+   |snmp_community
+   |snmp_username
+   |snmp_password
+   |snmp_port
+   |snmp_timeout
+   |snmp_version}x;
+# #############################################################################
+# Subroutines.
+# #############################################################################
+sub es { # Element start
+   my ( $name ) = @_;
+   print(("\t" x $indent++), "<$name>\n");
+   push @stack, $name;
+}
+
+sub ee { # Element end
+   my ( $sanity ) = @_;
+   my ( $name ) = pop @stack;
+   if ( ($sanity || $name) ne $name ) {
+      die "You tried to end the '$sanity' element but stack has '$name'";
+   }
+   print(("\t" x --$indent), "</$name>\n");
+}
+
+sub el { # Element plus contents
+   my ( $name, $content ) = @_;
+   print(("\t" x $indent), "<$name>", enc($content), "</$name>\n");
+}
+
+sub enc { # Entity encoding.
+   my ( $val ) = @_;
+   $val =~ s/&/&amp;/g;
+   $val =~ s/>/&gt;/g;
+   $val =~ s/</&lt;/g;
+   $val =~ s/"/&quot;/g;
+   return $val;
+}
+
+# Turn A_string_of_words into A String Of Words.
+sub to_words {
+   my ( $text ) = @_;
+   return join(' ', map { ucfirst } split(/_/, $text));
+}
+
+# Do the work.
+# #############################################################################
+
+es('cacti');
+
+# Print the beginning of the host template
+es($t->{hash});
+el('name', "$name_prefix $t->{name} HT");
+el('graph_templates',
+   join('|', map { $_->{hash} } @{$t->{graphs}} ));
+el('data_queries', '');
+ee($t->{hash});
+
+# Graph templates
+foreach my $g ( @{ $t->{graphs} } ) {
+   # Set the graph's title
+   $g->{title} = "|host_description| - $g->{name}";
+
+   # Print the graph template and the graph items
+   es($g->{hash});
+   el('name', "$name_prefix $g->{name} GT");
+   es('graph');
+   foreach my $p ( @graph_props ) {
+      el("t_$p", ''); # No idea what this is for, it's always empty.
+      el($p, $g->{$p} || $graph_props{$p} || '');
+   }
+   ee('graph');
+
+   # Now add the items (lines, gprints...) to the graph
+   es('items');
+   my $seq = 0;
+   foreach my $it ( @{ $g->{items} } ) {
+      my $i = 0;
+      my $color = $it->{color} || 0;
+      my $hash_count = scalar(@{ $it->{hashes} });
+
+      # Each bit of data can have several items.
+      foreach my $h ( @{ $it->{hashes} } ) {
+         my $type = $i ? 'GPRINT' : ($it->{type} || 'LINE1');
+         my $text = $i ? $graph_texts[$i] : to_words($it->{item});
+         es($h);
+         el('task_item_id', $g->{dt}->{$it->{item}}->{hash});
+         el('color_id', $i ? 0 : $color);
+         el('graph_type_id', $graph_types{$type});
+         el('consolidation_function_id', $consolidations[$i]);
+         el('cdef_id', 0);
+         el('value', '');
+         el('gprint_id',
+            $t->{gprints}->{ $it->{gprint_id} || 'Normal' }->{hash});
+         el('text_format', $text);
+         el('hard_return', ($i == $hash_count - 1) ? 'on' : '');
+         el('sequence', ++$seq);
+         ee($h);
+         $i++;
+      }
+   }
+   ee('items');
+
+   # Now add the inputs.
+   es('inputs');
+   foreach my $it ( @{ $g->{items} } ) {
+      es($it->{task});
+      el('name', "Data Source [$it->{item}]");
+      el('description', '');
+      el('column_name', 'task_item_id');
+      # TODO: it looks like this is not exported correctly in my version of
+      # Cacti, so it might not matter at all.
+      el('items', '');
+         # join('|', map { } 
+      ee($it->{task});
+   }
+   ee('inputs');
+
+   ee($g->{hash});
+}
+
+# Data templates
+foreach my $g ( @{ $t->{graphs} } ) {
+   my $d = $g->{dt};
+   es($d->{hash});
+   my $i = $t->{inputs}->{$d->{input}};
+
+   # The <ds> data source element.
+   el('name', "$name_prefix $g->{name} DT");
+   es('ds');
+   el('t_name', '');
+   el('name', "|host_description| - $g->{name}");
+   el('data_input_id', $t->{inputs}->{$d->{input}}->{hash} );
+   el('t_rra_id', '');
+   el('t_rrd_step', '');
+   el('rrd_step', $rrd_step);
+   el('t_active', '');
+   el('active', 'on');
+   el('rra_items', join('|', map { $_->{hash} } @{ $t->{rras} }) );
+   ee('ds');
+
+   # The items that go into the RRA.
+   es('items');
+   foreach my $k ( keys %$d ) {
+      my $v = $d->{$k};
+      next unless ref $v eq 'HASH';
+      $v->{data_source_name} = $k;
+      $v->{data_input_field_id} = $i->{outputs}->{$k};
+      es($v->{hash});
+      foreach my $prop ( @ds_props ) {
+         el("t_$prop", '');
+         el($prop, $v->{$prop} || $ds_props{$prop});
+      }
+      ee($v->{hash});
+   }
+   ee('items');
+
+   # The inputs to the script that fills the RRA.
+   es('data');
+   my $cnt = 0;
+   foreach my $input ( @{ $i->{inputs} } ) {
+      es(sprintf('item_%03d', $cnt));
+      el('data_input_field_id', $input->{hash});
+      el('t_value', '');
+      el('value', '');
+      ee(sprintf('item_%03d', $cnt));
+      $cnt++;
+   }
+   ee('data');
+
+   ee($d->{hash});
+}
+
+# Input script definitions
+foreach my $k ( keys %{$t->{inputs}} ) {
+   my $v = $t->{inputs}->{$k};
+   es($v->{hash});
+   el('name', "$name_prefix $v->{name}");
+   el('type_id', $v->{type_id});
+   el('input_string', $v->{input_string});
+
+   es('fields');
+
+   # Input fields (arguments) to the script
+   foreach my $i ( @{$v->{inputs}} ) {
+      es($i->{hash});
+      el('name', to_words($i->{name}));
+      el('update_rra', '');
+      el('regexp_match', '');
+      el('allow_nulls', $i->{allow_nulls});
+      el('type_code', ($i->{name} =~ m/$is_magic/ ? $i->{name} : ''));
+      el('input_output', 'in');
+      el('data_name', $i->{name});
+      ee($i->{hash});
+   }
+
+   # Data returned by the script
+   foreach my $k ( keys %{ $v->{outputs} } ) {
+      es($v->{outputs}->{$k});
+      el('name', to_words($k));
+      el('update_rra', 'on');
+      el('regexp_match', '');
+      el('allow_nulls', '');
+      el('type_code', '');
+      el('input_output', 'out');
+      el('data_name', $k);
+      ee($v->{outputs}->{$k});
+   }
+
+   ee('fields');
+   ee($v->{hash});
+}
+
+# GPRINT formats
+foreach my $k ( keys %{ $t->{gprints} } ) {
+   es($t->{gprints}->{$k}->{hash});
+   el('name', $k);
+   el('gprint_text', $t->{gprints}->{$k}->{gprint_text});
+   ee($t->{gprints}->{$k}->{hash});
+}
+
+# TODO: CDEF definitions.
+
+# RRA (Round-Robin Archive) definitions
+foreach my $r ( @{$t->{rras}} ) {
+   es($r->{hash});
+   el('name', "$name_prefix $r->{name} RRA");
+   foreach my $thing ( qw(x_files_factor steps rows timespan cf_items) ) {
+      el($thing, $r->{$thing});
+   }
+   ee($r->{hash});
+}
+
+ee('cacti');
