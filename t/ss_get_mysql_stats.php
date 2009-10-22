384f65a5dc84801ff3c0067c4241b6482e04c039
baron.schwartz <baron.schwartz@14bb9f40-0f49-0410-a1f6-25d4bd87bb24>
2009-10-22 02:28:18 +0000

    Add some tests for InnoDB plugin version of the SHOW INNODB STATUS

--- t/ss_get_mysql_stats.php
+++ t/ss_get_mysql_stats.php
@@ -17,6 +17,12 @@ is(
 );
 
 is(
+   make_bigint('EF861B144C'),
+   '1028747105356',
+   'make_bigint EF861B144C'
+);
+
+is(
    big_sub('74900191315', '1170664159'),
    '73729527156',
    'big_sub 1170664159 74900191315'
@@ -66,6 +72,52 @@ is_deeply(
        'rows_read'                 => '1911833505287',
    ),
    'samples/innodb-001.txt'
+);
+
+is_deeply(
+   get_innodb_array(file_get_contents('samples/innodb-002.txt')),
+   array(
+       'spin_waits'                => '88127914',
+       'spin_rounds'               => '157459864',
+       'os_waits'                  => '4329779',
+       'pending_normal_aio_reads'  => '1',
+       'pending_normal_aio_writes' => '2',
+       'pending_ibuf_aio_reads'    => '1',
+       'pending_aio_log_ios'       => '2',
+       'pending_aio_sync_ios'      => '3',
+       'pending_log_flushes'       => '1',
+       'pending_buf_pool_flushes'  => '2',
+       'file_reads'                => '8782182',
+       'file_writes'               => '15635445',
+       'file_fsyncs'               => '947800',
+       'ibuf_inserts'              => '17549044',
+       'ibuf_merged'               => '15956910',
+       'ibuf_merges'               => '1676050',
+       'innodb_lsn'                => '13093949495856',
+       'flushed_to'                => '13093948219327',
+       'pending_log_writes'        => '1',
+       'pending_chkp_writes'       => '2',
+       'log_writes'                => '3430041',
+       'pool_size'                 => '1769471',
+       'free_pages'                => '1',
+       'database_pages'            => '1696503',
+       'modified_pages'            => '160602',
+       'pages_read'                => '15240822',
+       'pages_created'             => '1770238',
+       'pages_written'             => '21705836',
+       'queries_inside'            => '1',
+       'queries_queued'            => '2',
+       'read_views'                => '1',
+       'rows_inserted'             => '50678311',
+       'rows_updated'              => '66425915',
+       'rows_deleted'              => '20605903',
+       'rows_read'                 => '454561562',
+       'innodb_transactions'       => '1028747105356',
+       'unpurged_txns'             => '1026497183983',
+       'history_list'              => '132',
+       'current_transactions'      => '2',
+   ),
+   'samples/innodb-002.txt'
 );
 
 ?>
