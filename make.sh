1fb851a919f833baae71f51c94d7c81a6e7d1bfa
baron.schwartz <baron.schwartz@14bb9f40-0f49-0410-a1f6-25d4bd87bb24>
2009-10-25 20:26:13 +0000

    change name of package file to better-cacti-templates

--- make.sh
+++ make.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -x
 
-NAME=mysql-cacti-templates
+NAME=better-cacti-templates
 VERSION=`grep version Changelog | head -n 1 | cut -d ' ' -f 3`;
 DISTFILES="COPYING README ss_get_mysql_stats.php Changelog meta/make-template.pl meta/mysql_definitions.pl"
 DISTFILES="${DISTFILES} ss_get_by_ssh.php meta/apache_definitions.pl meta/gnu_linux_definitions.pl meta/memcached_definitions.pl meta/nginx_definitions.pl"
